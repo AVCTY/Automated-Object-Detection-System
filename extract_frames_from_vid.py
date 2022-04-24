7c7923df6f77fc8d009ded25e1954b53e3bf072a
CGYJ <63357110+klamon5240@users.noreply.github.com>
2022-04-24 23:08:33 +0800

    Update extract_frames_from_vid.py and remove extracted folder

--- extract_frames_from_vid.py
+++ extract_frames_from_vid.py
@@ -5,13 +5,14 @@ import glob #if this module not found use <pip install glob2>
 #Creating folder to store extracted frames
 try:
     # creating a folder named data
-    if not os.path.exists('extracted'):
-        os.makedirs('extracted')
+    if not os.path.exists('../Dataset/extracted/20201127'):
+        os.makedirs('../Dataset/extracted/20201127')
+
 # if not created then raise error
 except OSError:
     print ('Error: Creating directory named extracted')
 
-video_folder = "../Dataset/To extract/20201113_new" #change your folder containing videos here
+video_folder = "../Dataset/To extract/20201127" #change your folder containing videos here
 video_files = glob.glob(f"{video_folder}/*.mp4")
 
 for video in video_files:
@@ -22,7 +23,7 @@ for video in video_files:
     count = 0 #current frame
     while success:
         vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*5000)) # set to 1 frame per 5 seconds
-        cv2.imwrite(f"extracted/{filename}_frame{count}.jpg", image)     # save frame as JPEG file      
+        cv2.imwrite(f"../Dataset/extracted/20201127/{filename}_frame{count}.jpg", image)     # save frame as JPEG file      
         success,image = vidcap.read()
         print('Read a new frame: ', success, f"File: extracted/{filename}_frame{count}.jpg")
         count += 1
